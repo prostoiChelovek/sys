30cd2260d5425022b405aae027cf2cbdcfc394a4
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2023-12-24 17:37:58 +0500

    download stuff

---
+++ get.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# https://stackoverflow.com/a/246128/9577873
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+cd $DIR
+
+MIRROR_URL="ftp://mirror.yandex.ru/slackware"
+URL="$MIRROR_URL/slackware-15.0/slackware"
+DST=./dst
+
+mkdir -p $DST
+
+cd $DST
+for soft_set in a ap d e f l n tcl x xap; do
+    mkdir -p $soft_set
+    cd $soft_set
+
+    echo "==================== $soft_set ===================="
+    wget -r -nc -np -nd --timeout 3 -nv --show-progress "$URL/$soft_set"
+
+    cd ..
+done
+cd ..
+
