d1758e529029a9f951e12e2f4696210b2fe31f45
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2024-03-06 16:42:31 +0000

    add dwm

--- programs/dwm.sh
+++ programs/dwm.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Run this in slackware
-
 set -e
 set -o pipefail
 
 cd $(dirname -- "$( readlink -f -- "$0"; )")
 
-./programs/imlib2.sh
-./programs/feh.sh
-./programs/dwm.sh
+cd ./dwm/
+
+make clean install
 
