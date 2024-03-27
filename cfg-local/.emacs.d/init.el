24ea8d77f37f5abf0438c8eb7d40f7d2aff39e90
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2024-03-28 00:16:50 +0500

    add evil-org and remove bars

--- cfg-local/.emacs.d/init.el
+++ cfg-local/.emacs.d/init.el
@@ -1,5 +1,18 @@
 (add-to-list 'load-path (expand-file-name "packages/evil/" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "packages/evil-org-mode/" user-emacs-directory))
 
 (require 'evil)
 (evil-mode 1)
+
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(load-theme 'manoj-dark)
 
